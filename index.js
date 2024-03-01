8edab85350cf80aa0f046ca33f520e15ca118864
parkermunson <parkermunson@gmail.com>
2024-03-01 13:21:03 -0500

    updated styles, html, and javascript

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function myFunction() {
-    document.getElementById("wall").style.backgroundImage = "url(assets/gallery.jpeg)";
- }
+function toggleContainer() {
+    var container = document.getElementById('container');
+    if (container.style.display === 'none') {
+        container.style.display = 'block';
+        toggleButton.textContent = toggleButton.textContent === "Apply Colors" ? "Remove Colors" : "Remove Colors";
+    } else {
+        container.style.display = 'none';
+        toggleButton.textContent = toggleButton.textContent === "Remove Colors" ? "Apply Colors" : "Remove Colors";
+    }
+}
